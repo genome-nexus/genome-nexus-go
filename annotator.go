70998fd559c8e74658940c8d00520430d8cbed82
averyniceday <wanga5@mskcc.org>
2024-03-27 11:20:27 -0700

    Update annotator.go

--- annotator.go
+++ annotator.go
@@ -2,8 +2,8 @@ package annotator
 
 import (
 	"context"
-	"fmt"
 	"os"
+	"log"
 
 	pubgn "github.com/averyniceday/go-mpath-proto/genome-nexus-public-api"
 )
@@ -24,20 +24,17 @@ func GetVariantAnnotations(genomicLocations []pubgn.GenomicLocation) []pubgn.Var
 	x = x.Token(token)
 	variantAnnotations, r, err := x.Execute()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationGET``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		log.Printf("Error when calling `AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationGET``: %v\n", err)
+		log.Printf(os.Stderr, "Full HTTP response: %v\n", r)
 	}
-	fmt.Println("sigh \n\n\n\n")
-	fmt.Println(r.Request)
-	fmt.Println(r.Header)
 	// https://github.com/averyniceday/go-mpath-proto/blob/16d6085cee926ad85fc29c6c62319ee3697edcf2/genome-nexus-public-api/api_annotation_controller.go#L218
 	// response from `FetchVariantAnnotationByGenomicLocationPOST`: []VariantAnnotation, *http.Response, error
 	// VariantAnnotation defined here: https://github.com/averyniceday/go-mpath-proto/blob/16d6085cee926ad85fc29c6c62319ee3697edcf2/genome-nexus-internal-api/model_variant_annotation.go#L21
 	for i := 0; i < len(variantAnnotations); i++ {
 		if !*variantAnnotations[i].SuccessfullyAnnotated {
-			fmt.Println("ERROR: Failed to annotate: ", variantAnnotations[i].Variant)
+			log.Printf("ERROR: Failed to annotate: ", variantAnnotations[i].Variant)
 		} else {
-			fmt.Println("Success: ", *variantAnnotations[i].SuccessfullyAnnotated)
+			log.Printf("Success: ", *variantAnnotations[i].SuccessfullyAnnotated)
 		}
 	}
 	return variantAnnotations
