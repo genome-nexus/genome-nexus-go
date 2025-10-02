4a68fbd24b38717dd6421f1b779e2e5e5a91b5a6
Benjamin Gross <benjamin.gross@gmail.com>
2025-10-02 15:07:54 -0400

    Update AnnotateTempoMessageEvents to continue processing annotations when an error is encountered

--- gnannotator_service.go
+++ gnannotator_service.go
@@ -47,7 +47,10 @@ func (gn GNAnnotatorService) GetGenomeNexusInfo() (*gnapi.AggregateSourceInfo, e
 	return resp, nil
 }
 
-func (gn GNAnnotatorService) AnnotateTempoMessageEvents(isoformOverrideSource string, tm *tt.TempoMessage) error {
+func (gn GNAnnotatorService) AnnotateTempoMessageEvents(
+	isoformOverrideSource string,
+	tm *tt.TempoMessage,
+) error {
 	// prepare genomic locations for annotation
 	genomicLocations := make([]gnapi.GenomicLocation, 0)
 	for _, a := range tm.Events {
@@ -60,15 +63,14 @@ func (gn GNAnnotatorService) AnnotateTempoMessageEvents(isoformOverrideSource st
 		return err
 	}
 	if len(variantAnnotations) != len(genomicLocations) {
-		return fmt.Errorf("Returned number of variant annotations does not match genomic locations submitted")
+		return fmt.Errorf(
+			"Returned number of variant annotations does not match genomic locations submitted",
+		)
 	}
 
 	// construct a list of annotated events using genome nexus response
 	for i, variantAnnotation := range variantAnnotations {
-		err = gn.mapResponseToEvent(variantAnnotation, genomicLocations[i], tm.Events[i])
-		if err != nil {
-			return err
-		}
+		gn.mapResponseToEvent(variantAnnotation, genomicLocations[i], tm.Events[i])
 	}
 	return nil
 }
@@ -85,60 +87,113 @@ func (gn GNAnnotatorService) getGenomicLocation(e *tt.Event) gnapi.GenomicLocati
 	return *gloc
 }
 
-func (gn GNAnnotatorService) getVariantAnnotations(isoformOverrideSource string, genomicLocations []gnapi.GenomicLocation) ([]gnapi.VariantAnnotation, error) {
+func (gn GNAnnotatorService) getVariantAnnotations(
+	isoformOverrideSource string,
+	genomicLocations []gnapi.GenomicLocation,
+) ([]gnapi.VariantAnnotation, error) {
 	fields := make([]string, 0)
 	fields = append(fields, "annotation_summary")
-	x := gn.client.AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationPOST(gn.ctxAccessToken).GenomicLocations(genomicLocations)
+	x := gn.client.AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationPOST(gn.ctxAccessToken).
+		GenomicLocations(genomicLocations)
 	x = x.Fields(fields)
 	x = x.IsoformOverrideSource(isoformOverrideSource)
 	x = x.Token(gn.token)
 
 	variantAnnotations, r, err := x.Execute()
 	if err != nil {
-		return variantAnnotations, fmt.Errorf("Error calling Genome Nexus annotation service: %v\nFull HTTP response: %v\n", err, r)
+		return variantAnnotations, fmt.Errorf(
+			"Error calling Genome Nexus annotation service: %v\nFull HTTP response: %v\n",
+			err,
+			r,
+		)
 	}
 	return variantAnnotations, nil
 }
 
-func (gn GNAnnotatorService) mapResponseToEvent(variantAnnotation gnapi.VariantAnnotation, genomicLocation gnapi.GenomicLocation, event *tt.Event) error {
+func (gn GNAnnotatorService) mapResponseToEvent(
+	variantAnnotation gnapi.VariantAnnotation,
+	genomicLocation gnapi.GenomicLocation,
+	event *tt.Event,
+) {
 	if !*variantAnnotation.SuccessfullyAnnotated {
-		event.AnnotationStatus = "FAILED"
-		return fmt.Errorf("Unsuccessful variant annotation for genomicLocation: %v", variantAnnotation)
+		event.AnnotationStatus = fmt.Sprintf(
+			"FAILURE: Unsuccessful variant annotation for genomicLocation: %v",
+			variantAnnotation,
+		)
+		return
 	}
 	canonicalTranscript := getCanonicalTranscript(variantAnnotation)
 	// Taken from GN response (default)
-	event.Chromosome = resolveChromosome(variantAnnotation, genomicLocation) // annotationUtil.resolveChromosome(gnResponse, mRecord)
+	event.Chromosome = resolveChromosome(
+		variantAnnotation,
+		genomicLocation,
+	) // annotationUtil.resolveChromosome(gnResponse, mRecord)
 	event.StartPosition = resolveStart(variantAnnotation, genomicLocation)
-	event.EndPosition = resolveEnd(variantAnnotation, genomicLocation) // annotationUtil.resolveEnd(gnResponse, mRecord)
-	event.Strand = resolveStrandSign(variantAnnotation)                // annotationUtil.resolveStrandSign(gnResponse, mRecord)
-	event.NcbiBuild = resolveAssemblyName(variantAnnotation)           // annotationUtil.resolveAssemblyName(gnResponse, mRecord)
+	event.EndPosition = resolveEnd(
+		variantAnnotation,
+		genomicLocation,
+	) // annotationUtil.resolveEnd(gnResponse, mRecord)
+	event.Strand = resolveStrandSign(
+		variantAnnotation,
+	) // annotationUtil.resolveStrandSign(gnResponse, mRecord)
+	event.NcbiBuild = resolveAssemblyName(
+		variantAnnotation,
+	) // annotationUtil.resolveAssemblyName(gnResponse, mRecord)
 	event.HugoSymbol = resolveHugoSymbol(canonicalTranscript)
 	event.EntrezGeneId = resolveEntrezGeneId(canonicalTranscript)
-	event.VariantClassification = resolveVariantClassification(canonicalTranscript, *event) // annotationUtil.resolveVariantClassification(gnResponse, canonicalTranscript, mRecord)
-	event.VariantType = resolveVariantType(variantAnnotation)                // annotationUtil.resolveVariantType(gnResponse)
-	event.DbsnpRs = resolveDbSnpRs(variantAnnotation)                        // 	annotationUtil.resolveDbSnpRs(gnResponse, mRecord)
-	event.Hgvsc = resolveHgvsc(canonicalTranscript)                          // annotationUtil.resolveHgvsc(canonicalTranscript)
-	event.Hgvsp = resolveHgvsp(canonicalTranscript)                          // annotationUtil.resolveHgvsp(canonicalTranscript)
-	event.HgvspShort = resolveHgvspShort(canonicalTranscript)                // annotationUtil.resolveHgvspShort(canonicalTranscript)
-	event.TranscriptId = resolveTranscriptId(canonicalTranscript)            // annotationUtil.resolveTranscriptId(canonicalTranscript)
-	event.Refseq = resolveRefSeq(canonicalTranscript)                        // annotationUtil.resolveRefSeq(canonicalTranscript)
+	event.VariantClassification = resolveVariantClassification(
+		canonicalTranscript,
+		*event,
+	) // annotationUtil.resolveVariantClassification(gnResponse, canonicalTranscript, mRecord)
+	event.VariantType = resolveVariantType(
+		variantAnnotation,
+	) // annotationUtil.resolveVariantType(gnResponse)
+	event.DbsnpRs = resolveDbSnpRs(
+		variantAnnotation,
+	) // 	annotationUtil.resolveDbSnpRs(gnResponse, mRecord)
+	event.Hgvsc = resolveHgvsc(
+		canonicalTranscript,
+	) // annotationUtil.resolveHgvsc(canonicalTranscript)
+	event.Hgvsp = resolveHgvsp(
+		canonicalTranscript,
+	) // annotationUtil.resolveHgvsp(canonicalTranscript)
+	event.HgvspShort = resolveHgvspShort(
+		canonicalTranscript,
+	) // annotationUtil.resolveHgvspShort(canonicalTranscript)
+	event.TranscriptId = resolveTranscriptId(
+		canonicalTranscript,
+	) // annotationUtil.resolveTranscriptId(canonicalTranscript)
+	event.Refseq = resolveRefSeq(
+		canonicalTranscript,
+	) // annotationUtil.resolveRefSeq(canonicalTranscript)
 	event.Codons = resolveCodonChange(canonicalTranscript)
 	event.Consequence = resolveConsequence(canonicalTranscript)
 	event.ProteinPosition = resolveProteinPosition(canonicalTranscript)
 	event.ExonNumber = resolveExon(canonicalTranscript)
 
 	// Taken from GN response (Polyphen)
-	event.PolyphenPrediction = resolvePolyphenPrediction(canonicalTranscript) // annotationUtil.resolvePolyphenPrediction(canonicalTranscript)
-	event.PolyphenScore = resolvePolyphenScore(canonicalTranscript)           // annotationUtil.resolvePolyphenScore(canonicalTranscript)
+	event.PolyphenPrediction = resolvePolyphenPrediction(
+		canonicalTranscript,
+	) // annotationUtil.resolvePolyphenPrediction(canonicalTranscript)
+	event.PolyphenScore = resolvePolyphenScore(
+		canonicalTranscript,
+	) // annotationUtil.resolvePolyphenScore(canonicalTranscript)
 
 	// Taken from GN response (SIFT)
-	event.SiftPrediction = resolveSiftPrediction(canonicalTranscript) // annotationUtil.resolveSiftPrediction(canonicalTranscript)
-	event.SiftScore = resolveSiftScore(canonicalTranscript)           // annotationUtil.resolveSiftScore(canonicalTranscript)
+	event.SiftPrediction = resolveSiftPrediction(
+		canonicalTranscript,
+	) // annotationUtil.resolveSiftPrediction(canonicalTranscript)
+	event.SiftScore = resolveSiftScore(
+		canonicalTranscript,
+	) // annotationUtil.resolveSiftScore(canonicalTranscript)
 
 	// ======================================
 	// separate special function needed
-	event.ReferenceAllele, event.TumorSeqAllele1, event.TumorSeqAllele2 = resolveRefAndTumorSeqAlleles(variantAnnotation, *event, "true")
+	event.ReferenceAllele, event.TumorSeqAllele1, event.TumorSeqAllele2 = resolveRefAndTumorSeqAlleles(
+		variantAnnotation,
+		*event,
+		"true",
+	)
 	// ======================================
 	event.AnnotationStatus = "SUCCESS"
-	return nil
 }
